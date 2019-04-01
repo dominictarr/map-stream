14e8b2e171cb2c5743c9938e00bdbc671e506d46
MGardner02 <mgardner32@student.umuc.edu>
2019-04-01 17:34:49 -0400

    fixing code format to match

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ var Stream = require('stream').Stream
 
 module.exports = function (mapper, opts) {
 
-  var stream = new Stream(),
-    inputs = 0,
-    outputs = 0,
-    ended = false,
-    paused = false,
-    destroyed = false,
-    lastWritten = 0,
-    inNext = false
+  var stream = new Stream()
+    , inputs = 0
+    , outputs = 0
+    , ended = false
+    , paused = false
+    , destroyed = false
+    , lastWritten = 0
+    , inNext = false
 
   opts = opts || {};
   var errorEventName = opts.failures ? 'failure' : 'error';
@@ -33,15 +33,15 @@ module.exports = function (mapper, opts) {
   stream.writable = true
   stream.readable = true
 
-  function queueData(data, number) {
+  function queueData (data, number) {
     var nextToWrite = lastWritten + 1
 
-    if (number === nextToWrite) {
+    if(number === nextToWrite) {
       do {
         var dataToWrite;
 
         // write data contents only for the first iteration
-        if (number === nextToWrite) {
+        if(number === nextToWrite) {
           dataToWrite = data
         } else {
           dataToWrite = writeQueue[nextToWrite]
@@ -49,14 +49,14 @@ module.exports = function (mapper, opts) {
         }
 
         // If it's next, and its not undefined write it
-        if (dataToWrite !== undefined) {
+        if(dataToWrite !== undefined) {
           stream.emit.apply(stream, ['data', dataToWrite])
         }
 
         lastWritten++
         nextToWrite++
         outputs++
-        
+
         // If the next value is in the queue, keeping writing from the queue
       } while (writeQueue.hasOwnProperty(nextToWrite))
 
@@ -65,21 +65,21 @@ module.exports = function (mapper, opts) {
       writeQueue[number] = data
     }
 
-    if (inputs === outputs) {
-      if (paused) paused = false, stream.emit('drain') //written all the incoming events
-      if (ended) end()
+    if(inputs === outputs) {
+      if(paused) paused = false, stream.emit('drain') //written all the incoming events
+      if(ended) end()
     }
   }
 
-  function next(err, data, number) {
-    if (destroyed) return
+  function next (err, data, number) {
+    if(destroyed) return
     inNext = true
 
-    if (!err || opts.failures) {
+    if(!err || opts.failures) {
       queueData(data, number)
     }
 
-    if (err) {
+    if(err) {
       stream.emit.apply(stream, [errorEventName, err]);
     }
 
@@ -88,14 +88,14 @@ module.exports = function (mapper, opts) {
 
   // Wrap the mapper function by calling its callback with the order number of
   // the item in the stream.
-  function wrappedMapper(input, number, callback) {
+  function wrappedMapper (input, number, callback) {
     return mapper.call(null, input, function (err, data) {
       callback(err, data, number)
     })
   }
 
   stream.write = function (data) {
-    if (ended) throw new Error('map stream is not writable')
+    if(ended) throw new Error('map stream is not writable')
     inNext = false
     inputs++
 
@@ -107,26 +107,26 @@ module.exports = function (mapper, opts) {
     } catch (err) {
       //if the callback has been called syncronously, and the error
       //has occured in an listener, throw it again.
-      if (inNext)
+      if(inNext)
         throw err
       next(err)
       return !paused
     }
   }
 
-  function end(data) {
+  function end (data) {
     //if end was called with args, write it, 
     ended = true //write will emit 'end' if ended is true
     stream.writable = false
-    if (data !== undefined) {
+    if(data !== undefined) {
       return queueData(data, inputs)
-    } else if (inputs == outputs) { //wait for processing 
+    } else if(inputs == outputs) { //wait for processing 
       stream.readable = false, stream.emit('end'), stream.destroy()
     }
   }
 
   stream.end = function (data) {
-    if (ended) return
+    if(ended) return
     end(data)
   }
 
